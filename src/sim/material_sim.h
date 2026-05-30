9ec2bd2992e137ae87d540b5754118819cfa4af9
Andrew <andrewboudreau@gmail.com>
2026-05-30 12:29:39 -0500

    Add C material simulation interface

---
+++ src/sim/material_sim.h
@@ -0,0 +1,30 @@
+#ifndef MATERIAL_SIM_H
+#define MATERIAL_SIM_H
+
+#include <stdbool.h>
+
+#define SIM_WIDTH 128
+#define SIM_HEIGHT 72
+#define SIM_CELL_COUNT (SIM_WIDTH * SIM_HEIGHT)
+
+typedef struct MaterialSim {
+    float red[SIM_CELL_COUNT];
+    float blue[SIM_CELL_COUNT];
+    float nextRed[SIM_CELL_COUNT];
+    float nextBlue[SIM_CELL_COUNT];
+    float rollerAngle;
+    float rollerSpeed;
+    float diffusion;
+    float gap;
+    bool paused;
+} MaterialSim;
+
+void MaterialSim_Init(MaterialSim *sim);
+void MaterialSim_Reset(MaterialSim *sim);
+void MaterialSim_Step(MaterialSim *sim, float dt);
+void MaterialSim_AddPigment(MaterialSim *sim, float normalizedX, float normalizedY, float red, float blue, float radius);
+int MaterialSim_Index(int x, int y);
+float MaterialSim_RedAt(const MaterialSim *sim, int x, int y);
+float MaterialSim_BlueAt(const MaterialSim *sim, int x, int y);
+
+#endif

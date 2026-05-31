50f53890a42c863f507218aa83dc7dce7f9563ba
Andrew <andrewboudreau@gmail.com>
2026-05-30 19:12:22 -0500

    Model material mass and velocity per cell

--- src/sim/material_sim.h
+++ src/sim/material_sim.h
@@ -8,8 +8,12 @@
 #define SIM_CELL_COUNT (SIM_WIDTH * SIM_HEIGHT)
 
 typedef struct MaterialSim {
+    float material[SIM_CELL_COUNT];
     float red[SIM_CELL_COUNT];
     float blue[SIM_CELL_COUNT];
+    float vx[SIM_CELL_COUNT];
+    float vy[SIM_CELL_COUNT];
+    float nextMaterial[SIM_CELL_COUNT];
     float nextRed[SIM_CELL_COUNT];
     float nextBlue[SIM_CELL_COUNT];
     float rollerAngle;
@@ -24,6 +28,7 @@ void MaterialSim_Reset(MaterialSim *sim);
 void MaterialSim_Step(MaterialSim *sim, float dt);
 void MaterialSim_AddPigment(MaterialSim *sim, float normalizedX, float normalizedY, float red, float blue, float radius);
 int MaterialSim_Index(int x, int y);
+float MaterialSim_MaterialAt(const MaterialSim *sim, int x, int y);
 float MaterialSim_RedAt(const MaterialSim *sim, int x, int y);
 float MaterialSim_BlueAt(const MaterialSim *sim, int x, int y);
 
